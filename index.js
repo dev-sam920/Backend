1a719fc9cbad9bd9b36716288bb5b91a765d56cc
olaleye <olaleyesamuel426@gmail.com>
2026-03-26 23:12:16 +0100

    fffff

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose
 
 
 app.use(cors({
-  origin: ['https://your-frontend.vercel.app', 'http://localhost:3000'],
+  origin: ['https://receipt-keeper-frontend.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
   credentials: true
 }))
 app.use(express.json());
